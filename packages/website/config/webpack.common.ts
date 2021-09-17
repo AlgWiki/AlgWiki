fae178bb6d12c3bcbbdd45bdb1857900fb01f26a
acheronfail <acheronfail@gmail.com>
2021-09-17 23:09:14 +1000

    feat: initial setup for markdown editor

--- packages/website/config/webpack.common.ts
+++ packages/website/config/webpack.common.ts
@@ -36,8 +36,15 @@ export const common: webpack.Configuration = {
         exclude: /node_modules/,
       },
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.(c|s[ac])ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
     ],
   },
